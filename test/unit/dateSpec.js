f8ef47d60f5372c5fe542bc1fbbf2a6e8e020b30
ferron <ferronrsmith@gmail.com>
2013-08-20 22:33:45 -0800

    update spec to be timezone agnostic during testing

--- test/unit/dateSpec.js
+++ test/unit/dateSpec.js
@@ -5,49 +5,46 @@ describe('Testing Date.js library', function () {
 
     //Update : Added this specific bit of code to resolve the timezone difference between
     // the CI server and my local machine pre-fixed date
-    var _date = new Date(1370072749148);
-    var _userOffset = _date.getTimezoneOffset()*60*1000; // user's offset time
-    var _centralOffset = 240*60*1000; // 4 for AST - use whatever you need
-    var date = new Date(_date.getTime() - _userOffset + _centralOffset);
+    var date = new Date(1370072749148);
 
     it('Test Year conversion', function () {
-        expect(date.format('yyyy')).toBe('2013');
+        expect(date.format('yyyy')).toBe(utilDateTest('year', date));
     });
 
     it('Test Standard Date conversion', function () {
-        expect(date.format('mmm dd yy hh:m:ss a/p')).toBe('Jun 01 13 03:45:49 am');
+        expect(date.format('mmm dd yy hh:m:ss a/p')).toBe(utilDateTest('default', date));
     });
 
     it('Test Short Date conversion', function () {
-        expect(date.format('mm/dd/yy')).toBe('06/01/13');
+        expect(date.format('mm/dd/yy')).toBe(utilDateTest('sdate', date));
     });
 
     it('Test Medium Date conversion', function () {
-        expect(date.format('mmm dd, yyyy')).toBe('Jun 01, 2013');
+        expect(date.format('mmm dd, yyyy')).toBe(utilDateTest('fmonth', date));
     });
 
     it('Test Medium Date conversion', function () {
-        expect(date.format('mmm dd, yyyy')).toBe('Jun 01, 2013');
+        expect(date.format('mmm dd, yyyy')).toBe(utilDateTest('fmonth', date));
     });
 
     it('Test Medium Date (Full-Month Representation) conversion', function () {
-        expect(date.format('mmmm dd, yyyy')).toBe('June 01, 2013');
+        expect(date.format('mmmm dd, yyyy')).toBe(utilDateTest('fmonth', date));
     });
 
     it('Test Full Date conversion', function () {
-        expect(date.format('dddd, mmmm dd, yyyy')).toBe('Saturday, June 01, 2013');
+        expect(date.format('dddd, mmmm dd, yyyy')).toBe(utilDateTest('fdate', date));
     });
 
     it('Test Short Time conversion', function () {
-        expect(date.format('hh:m a/p')).toBe('03:45 am');
+        expect(date.format('hh:m a/p')).toBe(utilDateTest('stime', date));
     });
 
     it('Test Medium Time conversion', function () {
-        expect(date.format('hh:m:ss a/p')).toBe('03:45:49 am');
+        expect(date.format('hh:m:ss a/p')).toBe(utilDateTest('smed', date));
     });
 
     it('Test ISO Date Format conversion', function () {
-        expect(date.format('yyyy-mm-dd')).toBe('2013-06-01');
+        expect(date.format('yyyy-mm-dd')).toBe(utilDateTest('short', date));
     });
 
     it('Test Parse Functionality #1 ', function () {
@@ -87,3 +84,41 @@ describe('Test Date.js utility methods', function () {
     });
 
 });
+
+/**
+ *
+ * @param form {String}
+ * @param date {Date}
+ * @returns {string}
+ */
+function utilDateTest(form, date) {
+    if(form === 'short') {
+       return date.getFullYear() + "-" + (date.getMonth()+1).zf(2) + "-" + date.getDate();
+    } else if (form  === 'smed') {
+        return date.getHours() + ":" + date.getMinutes() + ":" +
+            date.getSeconds() + " " +
+            (date.getHours() < 12 ? 'am' : 'pm');
+    } else if (form === 'stime') {
+        return date.getHours() + ":" + date.getMinutes() + " " +
+            (date.getHours() < 12 ? 'am' : 'pm');
+    } else if (form === 'fdate') {
+        return gsDayNames[date.getDay()] + ", " + gsMonthNames[date.getMonth()] + " " +
+            date.getDate() + ", " + date.getFullYear();
+    } else if (form === 'fmonth') {
+        return gsMonthNames[date.getMonth()] + " " +
+            date.getDate() + ", " + date.getFullYear();
+    } else if (form === 'sdate') {
+        return (date.getMonth()+1).zf(2) + "/" + date.getDate() + "/" + date.getFullYear().substr(2);
+    } else if (form === 'year') {
+        return date.getFullYear().toString();
+    } else if (form === 'default') {
+        return gsMonthNames[date.getMonth()] + " " +
+            date.getDate() + " " + date.getFullYear().substr(2) + " " +
+            date.getHours() + ":" + date.getMinutes() + ":" +
+            date.getSeconds() + " " +
+            (date.getHours() < 12 ? 'am' : 'pm');
+    }
+
+    return "";
+
+}
